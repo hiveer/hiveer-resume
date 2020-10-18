50220f927fcb2625bbbb40e9719e5b07f231005a
Hiveer <ljp.hiveer@gmail.com>
2020-10-18 11:32:31 +0800

    define each pages

---
+++ src/pages.js
@@ -0,0 +1,39 @@
+export const Home = () => {
+  return (
+    <section className="home">
+      <h1>[Home Page]</h1>
+    </section>
+  )
+}
+
+export const About = () => {
+  return (
+    <section className="events">
+      <h1>[About the Campany]</h1>
+    </section>
+  )
+}
+
+export const Events = () => {
+  return (
+    <section className="events">
+      <h1>[Events Calender]</h1>
+    </section>
+  )
+}
+
+export const Products = () => {
+  return (
+    <section className="products">
+      <h1>[Products Catalog]</h1>
+    </section>
+  )
+}
+
+export const Contact = () => {
+  return (
+    <section className="contact">
+      <h1>[Contact Us]</h1>
+    </section>
+  )
+}
