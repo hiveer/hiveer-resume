c735c198fcf0588b15576567a239c996d9393bda
Hiveer <ljp.hiveer@gmail.com>
2020-10-20 14:39:34 +0800

    Set up the head portrait

--- src/components/main_menu.js
+++ src/components/main_menu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import './main_menu.css'
+import headportrait from './head_portrait.png'
 
 const selectedStyle = {
   backgroundColor: "white",
@@ -12,29 +13,33 @@ export const MainMenu = () => {
   return(
     <nav className="main-menu">
       <p>
+        <img className="head-portrait" src={headportrait} />
+      </p>
+
+      <p className="main-menu-nav">
         <NavLink to="/">
         </NavLink>
       </p>
 
-      <p>
+      <p className="main-menu-nav">
         <NavLink to="/about" activeStyle={selectedStyle}>
           [About]
         </NavLink>
       </p>
 
-      <p>
+      <p className="main-menu-nav">
         <NavLink to="/events" activeStyle={selectedStyle}>
           [Events]
         </NavLink>
       </p>
 
-      <p>
+      <p className="main-menu-nav">
         <NavLink to="/products" activeStyle={selectedStyle}>
           [Products]
         </NavLink>
       </p>
 
-      <p>
+      <p className="main-menu-nav">
         <NavLink to="/contact" activeStyle={selectedStyle}>
           [Contact Us]
         </NavLink>
--- src/components/page_template.js
+++ src/components/page_template.js
@@ -8,11 +8,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export const PageTemplate = ({children}) => {
   return (
     <div className="page">
-      <div class="row row-no-gutters height100">
-        <div class="col-xs-3 col-md-3 height100 left-sidebar">
+      <div className="row row-no-gutters height100">
+        <div className="col-xs-3 col-md-3 height100 left-sidebar">
           <MainMenu />
         </div>
-        <div class="col-xs-9 col-md-9 height100 right-page-content">
+        <div className="col-xs-9 col-md-9 height100 right-page-content">
           {children}
         </div>
       </div>
