4f07372dbbe0cbac6f78e467456b1d4d64e9c791
Hiveer <ljp.hiveer@gmail.com>
2020-10-20 13:46:07 +0800

    introduce bootstrap

--- src/components/page_template.js
+++ src/components/page_template.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 import { MainMenu } from './main_menu.js'
 
+import './page_template.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export const PageTemplate = ({children}) => {
   return (
     <div className="page">
-      <MainMenu />
-      {children}
+      <div class="row row-no-gutters">
+        <div class="col-xs-5 col-md-5">
+          <MainMenu />
+        </div>
+        <div class="col-xs-7 col-md-7">
+          {children}
+        </div>
+      </div>
     </div>
   )
 }
