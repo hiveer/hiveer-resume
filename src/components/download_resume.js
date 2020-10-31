9da1bdf2b135d955f452ae61d7d12579515aedbb
Hiveer <ljp.hiveer@gmail.com>
2020-10-31 22:24:26 +0800

    download resume in pdf

--- src/components/download_resume.js
+++ src/components/download_resume.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { PageTemplate } from './page_template.js'
 
+import resume from './resume.pdf'
+import resume_png from './resume.png'
+
+import './download_resume.css'
+
 export const DownloadResume = () => {
   return (
     <PageTemplate>
-      <section className="events">
-        <h1>Download Resume</h1>
+      <section className="download-resume">
+        <div>
+          <img className="download-resume__img--resume-png" src={resume_png} />
+        </div>
+        <div>
+          <a href={resume}>
+            <button>
+              Download resume
+            </button>
+          </a>
+        </div>
       </section>
     </PageTemplate>
   )
