9ba62925c3d79388d2d0b9342f9f743c44ace0bd
Hiveer <ljp.hiveer@gmail.com>
2021-02-17 17:18:46 +0800

    optimize the footerNav style

--- src/components/footerNav/footerNav.js
+++ src/components/footerNav/footerNav.js
@@ -4,21 +4,26 @@ import { PageTemplate } from './../pageTemplate/pageTemplate.js'
 
 import './footerNav.css'
 
+const selectedStyle = {
+  backgroundColor: "white",
+  color: "slategray"
+}
+
 export const FooterNav = () => {
   return (
     <div className="container" id="mobile-footer-nav">
       <div className="row">
         <div className="col">
-          <NavLink className="nav-link" to="/about-me">我</NavLink>
+          <NavLink className="nav-link" to="/about-me" activeStyle={selectedStyle}>我</NavLink>
         </div>
         <div className="col">
-          <NavLink className="nav-link" to="/work-experiences">工作</NavLink>
+          <NavLink className="nav-link" to="/work-experiences" activeStyle={selectedStyle}>工作</NavLink>
         </div>
         <div className="col">
-          <NavLink className="nav-link" to="/project-experiences">项目</NavLink>
+          <NavLink className="nav-link" to="/project-experiences" activeStyle={selectedStyle}>项目</NavLink>
         </div>
         <div className="col">
-          <NavLink className="nav-link" to="/download-resume">简历</NavLink>
+          <NavLink className="nav-link" to="/download-resume" activeStyle={selectedStyle}>简历</NavLink>
         </div>
       </div>
     </div>
