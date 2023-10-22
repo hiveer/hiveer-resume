8f01f80e5d3cb601f33e4152b6860013d4e17dec
Hiveer <ljp.hiveer@gmail.com>
2023-10-22 17:42:17 +0800

    Reinstall baidu tongji code

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?58da99ddd6f85f95efec8cb44e6ba140";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
